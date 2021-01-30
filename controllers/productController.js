a5143c11de7e6332c7ee238ddae83c25c69d97a9
xzc9607 <297243970@qq.com>
2021-01-30 17:42:31 +0800

    2

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,19 @@
 var dbConfig = require('../util/dbconfig');
 
 //获取产品列表
+getNewProductlist = (req, res) => {
+    var sql = 'SELECT * FROM product ORDER BY pid DESC LIMIT 50';
+    var sqlArr = [];
+    var callBack = (err, data) => {
+        if (err) {
+            console.log('连接出错')
+        } else {
+            res.send(data);
+        }
+    }
+    dbConfig.sqlConnect(sql, sqlArr, callBack);
+}
+
 getProductlist = (req, res) => {
     var sql = 'select * from product';
     var sqlArr = [];
@@ -30,6 +43,7 @@ searchProduct = (req, res)=>{
 }
 
 module.exports={
+    getNewProductlist,
     getProductlist,
     searchProduct
 }
--- routes/product.js
+++ routes/product.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var product = require('../controllers/productController');
 
 router.get('/getProductlist', product.getProductlist);
+router.get('/getNewProductlist', product.getNewProductlist);
 router.get('/searchProduct',product.searchProduct);
 
 module.exports = router;
