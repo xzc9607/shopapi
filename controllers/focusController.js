203cd2f7db40d1400d8b819c96dc6acf557e1a6a
xzc9607 <297243970@qq.com>
2021-02-01 16:30:19 +0800

    接口优化

--- controllers/focusController.js
+++ controllers/focusController.js
@@ -14,6 +14,23 @@ getFocusListByUid=(req,res)=>{
     }
     dbConfig.sqlConnect(sql, sqlArr, callBack);
 }
+
+AddFocus=(req,res)=>{
+    let uid = req.body.uid;
+    let pid = req.body.pid;
+    var sql = 'INSERT INTO focus (uid,pid) VALUE (?,?)';
+    var sqlArr = [uid,pid];
+    var callBack = (err, data) => {
+        if (err) {
+            console.log('连接出错')
+        } else {
+            console.log(sqlArr);
+            res.send(data);
+        }
+    }
+    dbConfig.sqlConnect(sql, sqlArr, callBack);
+}
 module.exports={
     getFocusListByUid,
+    AddFocus,
 }
--- routes/focus.js
+++ routes/focus.js
@@ -3,5 +3,6 @@ var router = express.Router();
 var focus = require('../controllers/focusController');
 
 router.get('/getFocusListByUid',focus.getFocusListByUid);
+router.post('/addfocus',focus.AddFocus);
 
 module.exports = router;
